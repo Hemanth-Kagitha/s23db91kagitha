988cbd041295d43c4c1d23e3b8eb62a1a181fe46
Hemanth-Kagitha <123528403+Hemanth-Kagitha@users.noreply.github.com>
2023-04-24 19:50:50 -0500

    bonus, new buttons

--- models/kite.js
+++ models/kite.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
 const kiteSchema = mongoose.Schema({
-    Kite_color: String,
-    Kite_length: String,
-    Kite_cost: String,
-    Kite_sides: String
+    Kite_color: {type: String, minlength: 1, maxlength: 50},
+    Kite_length: {type: String, minlength: 1, maxlength: 50},
+    Kite_cost: {type: String, min: 03, maxlength: 3333},
+    Kite_sides: {type: String, minlength: 1, maxlength: 30}
 })
 module.exports = mongoose.model("Kite",kiteSchema)
+
