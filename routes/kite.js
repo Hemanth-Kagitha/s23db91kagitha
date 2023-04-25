205c352a32ac8d80b04442d5a564208f5a4fdeeb
Hemanth-Kagitha <123528403+Hemanth-Kagitha@users.noreply.github.com>
2023-04-24 19:50:32 -0500

    bonus, new buttons

--- routes/kite.js
+++ routes/kite.js
@@ -6,19 +6,29 @@ var router = express.Router();
 //router.get('/', function(req, res, next) {
   //res.render('kite', { title: 'Search Results Kite' });
 //});
+// A little function to check if we have an authorized user and continue on
 
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
 
 router.get('/', kite_controlers.kite_view_all_Page );
 /* GET detail kite page */
-router.get('/detail', kite_controlers.kite_view_one_Page);
+router.get('/detail',secured, kite_controlers.kite_view_one_Page);
 
 
 /* GET create kite page */
-router.get('/create', kite_controlers.kite_create_Page);
+router.get('/create', secured, kite_controlers.kite_create_Page);
 
-router.get('/update', kite_controlers.kite_update_Page);
+//router.get('/update', kite_controlers.kite_update_Page);
 
-router.get('/delete', kite_controlers.kite_delete_Page);
+router.get('/delete', secured,  kite_controlers.kite_delete_Page);
 
+router.get('/update', secured, kite_controlers.kite_update_Page);
 module.exports = router;
 
